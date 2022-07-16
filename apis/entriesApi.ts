4cf603354e4184be9c59c76d490eec3770e1041a
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-07-16 09:06:23 -0500

    [FIX] change host to request api for production

--- apis/entriesApi.ts
+++ apis/entriesApi.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const host = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://open-jira-fml.vercel.app'
+
 const entriesApi = axios.create({
-    baseURL: '/api'
+    baseURL: `${host}/api`
 })
 
 export default entriesApi
